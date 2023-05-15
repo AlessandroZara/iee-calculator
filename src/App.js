e739650e136d64e60f20cec91d24d64e69a439ff
Alessandro Zara <alezara@hotmail.it>
2023-05-15 15:10:35 +0200

    formula corrected

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 function App() {
   const [category, setCategory] = useState(
-    "1.Frigorifero con uno o piu' scomparti per la conservazione di alimenti freschi"
+    "1.Frigorifero Verticale"
   ); // stato per la categoria dell'apparecchio
   const [volume, setVolume] = useState(""); // stato per il volume dell'apparecchio
   const [energy, setEnergy] = useState(""); // stato per l'energia annua consumata dall'apparecchio
@@ -13,20 +13,14 @@ function App() {
   const [errorEnergy, setErrorEnergy] = useState(""); // stato per l'errore
   // Tabella dei valori di M ed N in base alla categoria dell'apparecchio
   const table = {
-    "1.Frigorifero con uno o piu' scomparti per la conservazione di alimenti freschi":
-      { M: 0.233, N: 245 },
-    "2.Frigorifero con scomparto a temperatura moderata, apparecchio a temperatura moderata, cantinetta":
-      { M: 0.233, N: 245 },
-    "3.Raffreddatore e Frigorifero con scomparto a 0 Stelle": {
-      M: 0.233,
-      N: 245,
-    },
-    "4.Frigorifero con scomparto a 1 Stella": { M: 0.643, N: 191 },
-    "5.Frigorifero con scomparto a 2 Stelle": { M: 0.45, N: 245 },
-    "6.Frigorifero con scomparto a 3 Stelle": { M: 0.777, N: 303 },
-    "7.Frigo-Congelatore": { M: 0.777, N: 303 },
-    "8.Congelatore Verticale": { M: 0.539, N: 315 },
-    "9.Congelatore a pozzetto": { M: 0.472, N: 286 },
+    "1.Frigorifero Verticale":
+      { M: 1.643, N: 609 },
+    "2.Congelatore Verticale":
+      { M: 4.928, N: 1472 },
+    "3.Tavolo Refrigerato": 
+    { M: 2.555, N: 1790, },
+    "4.Tavolo Congelatore": 
+    { M: 5.840, N: 2380 },
   };
 
   // Funzione per il calcolo di SAEc
@@ -39,7 +33,7 @@ function App() {
       const { M, N } = table[category]; // ottengo i valori di M ed N dalla tabella
       const CH = Veq > 15 ? 50 : 15; // calcolo il valore di CH in base al volume dell'apparecchio
       console.log(CH);
-      const SAEc = Veq * M + N + CH; // calcolo il valore di SAEc
+      const SAEc = Math.ceil(Veq * M + N + CH); // calcolo il valore di SAEc
       setSAEc(SAEc); // aggiorno lo stato di SAEc
       console.log(Veq);
     }
@@ -73,32 +67,17 @@ function App() {
           value={category}
           onChange={(e) => setCategory(e.target.value)}
         >
-          <option value="1.Frigorifero con uno o piu' scomparti per la conservazione di alimenti freschi">
-            1.Frigorifero con uno o piu' scomparti per la conservazione di
-            alimenti freschi
+          <option value="1.Frigorifero Verticale">
+          1.Frigorifero Verticale
           </option>
-          <option value="2.Frigorifero con scomparto a temperatura moderata, apparecchio a temperatura moderata, cantinetta">
-            2.Frigorifero con scomparto a temperatura moderata, apparecchio a
-            temperatura moderata, cantinetta
+          <option value="2.Congelatore Verticale">
+          2.Congelatore Verticale
           </option>
-          <option value="3.Raffreddatore e Frigorifero con scomparto a 0 Stelle">
-            3.Raffreddatore e Frigorifero con scomparto a 0 Stelle
+          <option value="3.Tavolo Refrigerato">
+          3.Tavolo Refrigerato
           </option>
-          <option value="4.Frigorifero con scomparto a 1 Stella">
-            4.Frigorifero con scomparto a 1 Stella
-          </option>
-          <option value="5.Frigorifero con scomparto a 2 Stelle">
-            5.Frigorifero con scomparto a 2 Stelle
-          </option>
-          <option value="6.Frigorifero con scomparto a 3 Stelle">
-            6.Frigorifero con scomparto a 3 Stelle
-          </option>
-          <option value="7.Frigo-Congelatore">7.Frigo-Congelatore</option>
-          <option value="8.Congelatore Verticale">
-            8.Congelatore Verticale
-          </option>
-          <option value="9.Congelatore a pozzetto">
-            9.Congelatore a pozzetto
+          <option value="4.Tavolo Congelatore">
+          4.Tavolo Congelatore
           </option>
         </select>
       </div>
